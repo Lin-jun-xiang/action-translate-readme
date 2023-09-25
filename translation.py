8aa9b6c6d536aaae549485df1a9d11e8be8c800b
hs_junxiang <jimmy871117@gmail.com>
2023-09-25 11:49:36 +0800

    Multi-LANGS & Remove PROVIDERS parameters
    1. add multi-langs version: pass parameters from .yml
    2. remove providers: use avaiableProvider and random choose.

--- translation.py
+++ translation.py
@@ -1,18 +1,42 @@
+import ast
 import os
+import random
 import subprocess
+from multiprocessing.dummy import Pool as ThreadPool
 
 import g4f
 from tenacity import retry, stop_after_attempt
 
-PROVIDER_MAPPING = {
-    f'g4f.Provider.{provider}': getattr(g4f.Provider, provider)
-    for provider in g4f.Provider.__all__
-}
+LAGNS = ast.literal_eval(os.environ.get('LANGS'))
 
-try:
-    PROVIDER = PROVIDER_MAPPING[os.environ.get('PROVIDER')]
-except:
-    PROVIDER = PROVIDER_MAPPING['g4f.Provider.DeepAi']
+
+class AvailableProviders:
+    providers = [getattr(g4f.Provider, provider) for provider in g4f.Provider.__all__]
+
+    @classmethod
+    def _test_provider(cls, provider: g4f.Provider) -> str:
+        try:
+            g4f.ChatCompletion.create(
+                model='gpt-3.5-turbo',
+                messages=[{"role": "user", "content": 'Hello world'}],
+                provider=provider
+            )
+            return provider
+        except:
+            return
+
+    @classmethod
+    def show(cls, threads_num: int = 4) -> list:
+        """Test all the providers then find out which are available"""
+        with ThreadPool(threads_num) as pool:
+            available_providers = pool.map(
+                cls._test_provider, cls.providers
+            )
+        return [
+            available_provider for available_provider in available_providers
+            if available_provider is not None
+        ]
+
 
 def run_shell_command(command):
     result = subprocess.run(
@@ -26,16 +50,18 @@ def run_shell_command(command):
 
 
 @retry(stop=stop_after_attempt(15))
-def chat_completion(query) -> str:
+def chat_completion(query, available_providers: list) -> str:
     return g4f.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": query}],
-        provider=PROVIDER # select provide which is active
+        provider=random.choice(available_providers)
     )
 
 
 def translate_content(content, output_lang):
     """Use GPT for translation"""
+    available_providers = AvailableProviders.show()
+
     translate_query = (
         f'翻譯以下 markdown 文本為{output_lang}語言，並且遵循以下規則:\n'
         '1. 保持文本原有格式、符號、空格數\n'
@@ -45,7 +71,7 @@ def translate_content(content, output_lang):
         '--------------------------------\n'
         f'{content}'
     )
-    response = chat_completion(translate_query)
+    response = chat_completion(translate_query, available_providers)
 
     check_query = (
         '原始問題與文本:\n'
@@ -54,7 +80,7 @@ def translate_content(content, output_lang):
         f'{response}\n'
         '請繼續完成未翻譯的部分，如果已經完成請回答None'
     )
-    check_response = chat_completion(check_query)
+    check_response = chat_completion(check_query, available_providers)
 
     if check_response != 'None':
         response = '\n' + check_response
@@ -66,22 +92,29 @@ def main():
     git_diff_command = "git diff --name-only HEAD~1 HEAD"
     return_code, stdout, stderr = run_shell_command(git_diff_command)
 
-    if return_code == 0:
-        modified_files = stdout.split("\n")
-        for file in modified_files:
-            if "README" in file:
-                output_lang = "en" if "zh-TW" in file else "zh-TW"
-                
-                with open(file, "r", encoding="utf-8") as f:
-                    content = f.read()
+    if return_code != 0:
+        print('no file changed.')
+        return
 
-                translated_content = translate_content(content, output_lang)
-                output_file = file.replace(".md", ".zh-TW.md") if output_lang == "zh-TW" else file.replace(".zh-TW", "")
+    modified_files = stdout.split("\n")
+    for file in modified_files:
+        if "README" not in file:
+            print('no README changed.')
+            return
 
-                with open(output_file, "w", encoding="utf-8") as f:
-                    f.write(translated_content)
-                    
-                print(f"Translated content written to {output_file}")
+        with open(file, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        for output_lang in LAGNS:
+            if output_lang in file:
+                continue
+            translated_content = translate_content(content, output_lang)
+            output_file = f'README.{output_lang}.md'
+            output_file = output_file.replace('.en', '')
+
+            with open(output_file, "w", encoding="utf-8") as f:
+                f.write(translated_content)
+            print(f"Translated content written to {output_file}")
 
 
 if __name__ == "__main__":
